clojure/clojure

commit fe4ed311166677cd571d23774171af1d830f7fc5

remove deprecated clojure.contrib.string
Since clojure.contrib.string is used by other submodules, some changes
were required:

- gen-html-docs and prxml needed changes because of functions were
  renamed or arguments reordered when promoted to clojure.string.
- jmx, json, miglayout, prxml and sql gained a private one-argument
  implementation of as-str.
- repl-utils gained a private copy of c.c.string/partition, named
  spartition.
- repl-utils replaced a call to c.c.string/substring? with a call to
  the java String method '.contains' (with swapped argument order).

Signed-off-by: Stuart Sierra <mail@stuartsierra.com>

modules/repl-utils/src/main/clojure/clojure/contrib/repl_utils.clj
@@ -18,23 +18,44 @@
   (:import (java.io File LineNumberReader InputStreamReader PushbackReader)
            (java.lang.reflect Modifier Method Constructor)
            (clojure.lang RT Compiler Compiler$C))
-  (:require [clojure.contrib.string :as s])
   (:use [clojure.contrib.seq :only (indexed)]
-        [clojure.java.browse :only (browse-url)]))
+        [clojure.java.browse :only (browse-url)]
+        [clojure.string :only (join)]))
 
 ;; ----------------------------------------------------------------------
 ;; Examine Java classes
 
+(defn- spartition
+  "Splits the string into a lazy sequence of substrings, alternating
+  between substrings that match the patthern and the substrings
+  between the matches.  The sequence always starts with the substring
+  before the first match, or an empty string if the beginning of the
+  string matches.
+
+  For example: (spartition #\"[a-z]+\" \"abc123def\")
+  returns: (\"\" \"abc\" \"123\" \"def\")"
+  [^Pattern re ^String s]
+  (let [m (re-matcher re s)]
+    ((fn step [prevend]
+       (lazy-seq
+        (if (.find m)
+          (cons (.subSequence s prevend (.start m))
+                (cons (re-groups m)
+                      (step (+ (.start m) (count (.group m))))))
+          (when (< prevend (.length s))
+            (list (.subSequence s prevend (.length s)))))))
+     0)))
+
 (defn- sortable [t]
   (apply str (map (fn [[a b]] (str a (format "%04d" (Integer. b))))
-                  (partition 2 (concat (s/partition #"\d+" t) [0])))))
+                  (spartition 2 (concat (spartition #"\d+" t) [0])))))
 
 (defn- param-str [m]
-  (str " (" (s/join
+  (str " (" (join
               "," (map (fn [[c i]]
                          (if (> i 3)
                            (str (.getSimpleName c) "*" i)
-                           (s/join "," (replicate i (.getSimpleName c)))))
+                           (join "," (replicate i (.getSimpleName c)))))
                        (reduce (fn [pairs y] (let [[x i] (peek pairs)]
                                                (if (= x y)
                                                  (conj (pop pairs) [y (inc i)])
@@ -138,7 +159,7 @@ str-or-pattern."
   [str-or-pattern]
   (let [matches? (if (instance? java.util.regex.Pattern str-or-pattern)
                    #(re-find str-or-pattern (str %))
-                   #(s/substring? (str str-or-pattern) (str %)))]
+                   #(.contains (str %) (str str-or-pattern)))]
     (mapcat (fn [ns]
               (filter matches? (keys (ns-publics ns))))
             (all-ns))))

modules/command-line/pom.xml
@@ -12,10 +12,5 @@
   </parent>
   <artifactId>command-line</artifactId>
   <dependencies>
-    <dependency>
-      <groupId>org.clojure.contrib</groupId>
-      <artifactId>string</artifactId>
-      <version>1.3.0-SNAPSHOT</version>
-    </dependency>
   </dependencies>
 </project>

modules/command-line/src/main/clojure/clojure/contrib/command_line.clj
@@ -12,7 +12,7 @@
     ^{:author "Chris Houser", 
        :doc "Process command-line arguments according to a given cmdspec"}
     clojure.contrib.command-line
-    (:use     (clojure.contrib [string :only (join)])))
+    (:use     (clojure [string :only (join)])))
 
 (defn make-map [args cmdspec]
   (let [{spec true [rest-sym] false} (group-by vector? cmdspec)

modules/complete/pom.xml
@@ -317,11 +317,6 @@
     </dependency>
     <dependency>
       <groupId>org.clojure.contrib</groupId>
-      <artifactId>string</artifactId>
-      <version>1.3.0-SNAPSHOT</version>
-    </dependency>
-    <dependency>
-      <groupId>org.clojure.contrib</groupId>
       <artifactId>strint</artifactId>
       <version>1.3.0-SNAPSHOT</version>
     </dependency>

modules/gen-html-docs/pom.xml
@@ -14,11 +14,6 @@
   <dependencies>
     <dependency>
       <groupId>org.clojure.contrib</groupId>
-      <artifactId>string</artifactId>
-      <version>1.3.0-SNAPSHOT</version>
-    </dependency>
-    <dependency>
-      <groupId>org.clojure.contrib</groupId>
       <artifactId>repl-utils</artifactId>
       <version>1.3.0-SNAPSHOT</version>
     </dependency>

modules/gen-html-docs/src/main/clojure/clojure/contrib/gen_html_docs.clj
@@ -46,7 +46,7 @@
      :doc "Generates a single HTML page that contains the documentation for
 one or more Clojure libraries."} 
   clojure.contrib.gen-html-docs
-  (:require [clojure.contrib.string :as s])
+  (:require [clojure.string :as s])
   (:use [clojure.contrib repl-utils def prxml])
   (:import [java.lang Exception]
 	   [java.util.regex Pattern]))
@@ -226,7 +226,7 @@ function toggle(targetid, linkid, textWhenOpen, textWhenClosed)
 	(if (= 0 (count l)) 
 	  [:span {:class "library-member-doc-whitespace"} " "] ; We need something here to make the blank line show up
 	  l)]) 
-     (s/split #"\n" docs)) 
+     (s/split docs #"\n"))
     ""))
 
 (defn- member-type 
@@ -270,7 +270,7 @@ function toggle(targetid, linkid, textWhenOpen, textWhenClosed)
 (defn- elide-to-one-line 
   "Elides a string down to one line."
   [s]
-  (s/replace-re #"(\n.*)+" "..." s))
+  (s/replace s #"(\n.*)+" "..."))
 
 (defn- elide-string 
   "Returns a string that is at most the first limit characters of s"
@@ -282,13 +282,9 @@ function toggle(targetid, linkid, textWhenOpen, textWhenClosed)
 (defn- doc-elided-src 
   "Returns the src with the docs elided."
   [docs src]
-  (s/replace-re (re-pattern (str "\"" (Pattern/quote docs) "\"")) 
-	  (str "\""
-		  (elide-to-one-line docs)
-;; 	          (elide-string docs 10)
-;;	          "..."
-		  "\"")
-	  src))
+  (s/replace src
+             (re-pattern (str "\"" (Pattern/quote docs) "\""))
+             (str "\"" (elide-to-one-line docs) "\"")))
 
 (defn- format-source [libid memberid v]
   (try

modules/jmx/pom.xml
@@ -17,10 +17,5 @@
       <artifactId>def</artifactId>
       <version>1.3.0-SNAPSHOT</version>
     </dependency>
-    <dependency>
-      <groupId>org.clojure.contrib</groupId>
-      <artifactId>string</artifactId>
-      <version>1.3.0-SNAPSHOT</version>
-    </dependency>
   </dependencies>
 </project>

modules/jmx/src/main/clojure/clojure/contrib/jmx.clj
@@ -72,7 +72,6 @@
   clojure.contrib.jmx
   (:refer-clojure :exclude [read])
   (:use clojure.contrib.def
-        [clojure.contrib.string :only [as-str]]
         [clojure.stacktrace :only (root-cause)]
         [clojure.walk :only [postwalk]])
   (:import [clojure.lang Associative]
@@ -82,6 +81,12 @@
 
 (defvar *connection* (ManagementFactory/getPlatformMBeanServer)
   "The connection to be used for JMX ops. Defaults to the local process.")
+
+(defn- as-str
+  [x]
+  (if (instance? clojure.lang.Named x)
+    (name x)
+    (str x)))
 
 (load "jmx/data")
 (load "jmx/client")

juergenhoetzel/clojure-contrib

commit 31c33107f59c71ec00ae1429b38f2005dce374d3

Fix invalid use of spartition instead of partition and add missing import
Regression bug was introduced by commit fe4ed311166677cd571d23774171af1d830f7fc5

modules/repl-utils/src/main/clojure/clojure/contrib/repl_utils.clj
@@ -17,6 +17,7 @@
   clojure.contrib.repl-utils
   (:import (java.io File LineNumberReader InputStreamReader PushbackReader)
            (java.lang.reflect Modifier Method Constructor)
+           (java.util.regex Pattern)
            (clojure.lang RT Compiler Compiler$C))
   (:use [clojure.contrib.seq :only (indexed)]
         [clojure.java.browse :only (browse-url)]
@@ -48,7 +49,7 @@
 
 (defn- sortable [t]
   (apply str (map (fn [[a b]] (str a (format "%04d" (Integer. b))))
-                  (spartition 2 (concat (spartition #"\d+" t) [0])))))
+                  (partition 2 (concat (spartition #"\d+" t) [0])))))
 
 (defn- param-str [m]
   (str " (" (join